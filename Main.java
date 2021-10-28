e890e6167dd46d7845a5aabd63db3a8bfdf81371
DiegoSarmiento4 <>
2021-10-28 00:10:05 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+*@author Diego Sarmiento
+*/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("La suma es:" +suma (2,2));
+  }
+  /**
+  *@param a Primer entero de la ecuacion
+  *@param a Segundo entero de la ecuacion
+  *@return La suma de los enteros a+b
+  */
+  public static int suma(int a, int b){
+    return a+b;
+  }
+}
